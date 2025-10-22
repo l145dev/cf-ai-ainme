222de77c449f397d5c04deb8d09d2ea6360c3614
Aryan <aryanshah0514@gmail.com>
2025-10-22 14:48:37 +0200

    feat: update to cloudflare-like theming

--- src/client/ChatUI.tsx
+++ src/client/ChatUI.tsx
@@ -12,7 +12,7 @@ const ChatUI = () => {
         <div className="relative">
           <TextareaAutosize
             placeholder="Ask anything about anime!"
-            className="min-w-[35vw] pb-12 flex w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            className="min-w-[35vw] min-h-20 pb-12 flex w-full rounded-xl border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50"
           />
           <Button
             className="absolute right-2 bottom-2"
